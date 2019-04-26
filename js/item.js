fccdbcbbb4683fc194443d236797066e6015e011
sjull <sjullieb@gmail.com>
2019-04-25 17:12:44 -0700

    add project files

---
+++ js/item.js
@@ -0,0 +1,14 @@
+export class Item{
+  constructor (id, name, imageUrl, description, colors, type, deliverable, dimentions, cost, stock){
+    this.id = id;
+    this.name = name;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.colors = colors;
+    this.type = type;
+    this.deliverable = deliverable;
+    this.dimentions = dimentions;
+    this.cost = cost;
+    this.stock = stock;
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,253 @@
+// import { MOCK } from "./mock.js";
+var availableTypes = [];
+var availableColors = [];
+var selectedTypes = [];
+var selectedColors = [];
+
+function loadFurniture(showError, availableColors, availableTypes){
+  console.log(1);
+  $.get(`https://it771mq5n2.execute-api.us-west-2.amazonaws.com/production/furniture`).then(function(response){
+  if(response.body.data.length > 0){
+    let data;
+    console.log(response.body.data);
+    for(let i=0; i < response.body.data.length; i++)
+    {
+      data = response.body.data[i];
+      console.log(data);
+      var colors = [];
+      for(j = 0; j < data.colors.length; j++)
+      {
+        if(!availableColors.includes(data.colors[j]))
+          availableColors.push(data.colors[j]);
+
+        colors.push();
+      }
+      // newDoctor = new Doctor(data.profile.last_name, data.profile.first_name, data.profile.image_url);
+      // allDoctors.push(newDoctor);
+      // let newPractice;
+      // let address;
+      // let newPatients;
+      // for(let k=0; k < data.practices.length; k++)
+      // {
+      //   address = data.practices[k].visit_address.street + " " + data.practices[k].visit_address.city + " " + data.practices[k].visit_address.state + " " + data.practices[k].visit_address.zip;
+      //
+      //   if(data.practices[k].accepts_new_patients.toString() == "true")
+      //   {
+      //     newPatients = "Yes";
+      //   }
+      //   else
+      //   {
+      //     newPatients = "No";
+      //   }
+      //
+      //   newPractice = new Practice(data.practices[k].name, address, newPatients);
+      //
+      //   for(let j=0; j < data.practices[k].phones.length; j++)
+      //   {
+      //     newPractice.addPhone(data.practices[k].phones[j].number);
+      //   }
+      //   newDoctor.addPractice(newPractice);
+      // }
+    }
+  }
+//  showDoctors(allDoctors);
+
+}).fail(function(error){
+  showError(error);
+});
+
+}
+
+function showData(dataArray, all, searchName, selectedTypes, selectedColors, deliverable, inStock){
+  var strHTML = "";
+  var show = true;
+  if(dataArray.length > 0){
+    console.log(dataArray);
+    for(let i=0; i < dataArray.length; i++)
+    {
+      var show = true;
+      let data = dataArray[i];
+      console.log(data);
+      if ((!all) && (name != "") && (!data.name.toUpperCase().includes(searchName.toUpperCase())))
+        show = false;
+
+      let colors = [];
+      var hasColors = false;
+      for(let j = 0; j < data.colors.length; j++)
+      {
+        //colors.push(data.colors[j]);
+        if((all) && (!availableColors.includes(data.colors[j])))
+          availableColors.push(data.colors[j]);
+        if((!all) && (selectedColors.includes(data.colors[j])))
+        {
+          hasColors = true;
+          break;
+        }
+      }
+
+      if((all) && (!availableTypes.includes(data.type)))
+        availableTypes.push(data.type);
+
+      if((!all) && ((!selectedTypes.includes(data.type)) || (!hasColors) || (deliverable != data.deliverable) || ((inStock && (data.stock == "0") ) || (!inStock && (data.stock != "0")))))
+        show = false;
+
+      if ((all) || (!all && show))
+      {
+        strHTML += "<div class='item' id='item" + i + "'>";
+        strHTML += "<img src='" + data.imageUrl + "' alt='a picture of " + data.name + "'>";
+        strHTML += "<p>" + data.name + " <span class='type'>" + data.type + "</span></p><p>$" + data.cost + "</p>";
+        if(data.stock == "0")
+        {
+          strHTML += "<p class='warning'>Out of stock</p>";
+        }
+        strHTML += "<div class='details'><p>" + data.description;
+        strHTML += "</p></div>";
+        strHTML += "</div>"; //class=item
+      }
+    }
+  }
+//  if(strHTML != "")
+  {
+    $(".grid").html(strHTML);
+  }
+}
+
+function attachListeners()
+{
+  $(".grid").on("click", "div", function()
+  {
+    console.log(this);
+    if(this.id.indexOf("item") != -1)
+    {
+      var i = parseInt(this.id.substring(4));
+      showItem(i);
+    }
+  });
+
+  $(".types input[type='checkbox']").change(function()
+  {
+    console.log($(this));
+    filterTypes($(this).val());
+  });
+
+  $(".colors input[type='checkbox']").change(function()
+  {
+    console.log($(this));
+    filterColors($(this).val());
+  });
+}
+
+function showItem(index)
+{
+  var data = MOCK.body.data[index];
+  var strHTML = "";
+  strHTML += "<img src='" + data.imageUrl + "' alt='a picture of " + data.name + "'>";
+  strHTML += "<p>" + data.name + " <span>" + data.type + "</span></p><p>$" + data.cost + "</p>";
+  if(data.stock == "0")
+  {
+    strHTML += "<p class='warning'>Out of stock</p>";
+  }
+  if(data.deliverable == false)
+  {
+    strHTML += "<p class='warning'>Not deliverable</p>";
+  }
+  if(data.dimensions)
+  {
+      strHTML += "<p>" + data.dimensions.length + " x " +data.dimensions.width + " sq.ft.</p>";
+  }
+
+  strHTML += "<div><p>" + data.description;
+  strHTML += "</p></div>";
+
+  $(".itemDetails").html(strHTML);
+}
+
+function filterColors(color)
+{
+  if(selectedColors.includes(color))
+  {
+    selectedColors.splice(selectedColors.indexOf(color), 1);
+  }
+  else
+  {
+    selectedColors.push(color);
+  }
+}
+
+function filterTypes(furnitureType)
+{
+  if(selectedTypes.includes(furnitureType))
+  {
+    selectedTypes.splice(selectedTypes.indexOf(furnitureType), 1);
+  }
+  else
+  {
+    selectedTypes.push(furnitureType);
+  }
+}
+
+function showError(error)
+{
+  $(".errors").text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+  $(".errors").show();
+}
+
+function showFilter()
+{
+  selectedTypes = [];
+  selectedColors = [];
+
+  var strHTML = "";
+
+  if(availableTypes.length > 0)
+  {
+    strHTML += "<div class='types'><label>Types</label><br>";
+    for(var i=0; i < availableTypes.length; i++)
+    {
+      strHTML += "<input type='checkbox' name='type' value='" + availableTypes[i] + "' checked>" + availableTypes[i] + "<br>";
+      selectedTypes.push(availableTypes[i]);
+    }
+    strHTML += "</div>";
+  }
+  if(availableColors.length > 0)
+  {
+    strHTML += "<div class='colors'><label>Colors</label><br>";
+    for(var i=0; i < availableColors.length; i++)
+    {
+      strHTML += "<input type='checkbox' name='color' value='" + availableColors[i] + "' checked>" + availableColors[i] + "<br>";
+      selectedColors.push(availableColors[i]);
+    }
+    strHTML += "</div>";
+  }
+
+  $(".dynamicFilter").html(strHTML);
+}
+
+$(document).ready(function() {
+  // $("#form").submit(function(event){
+  //   event.preventDefault();
+
+
+    showData(MOCK.body.data, true, "", selectedTypes, selectedColors, true, true);
+    showFilter();
+    attachListeners();
+  //  getFurniture(showError);
+  // })
+  $("#search").click(function(){
+    var searchName = $('#searchName').val();
+    var deliverable = $("#deliverable").prop("checked");
+    var inStock = $("#inStock").prop("checked");
+    showData(MOCK.body.data, false, $("#searchName").val(), selectedTypes, selectedColors, $("#deliverable").prop("checked"), $("#inStock").prop("checked"));
+  });
+
+  $("#clear").click(function(){
+    selectedTypes = [];
+    selectedIndex = [];
+    $("#inStock").checked = true;
+    $("#deliverable").checked = true;
+    $("#searchName").val("");
+    showData(MOCK.body.data, true, "", selectedTypes, selectedColors, true, true);
+    showFilter();
+  });
+
+});
